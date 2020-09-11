ac53807c6f1244c1e6bfa0a1e7e88430d20400be
Nikola Milić <milic.n96@gmail.com>
2020-09-11 10:38:02 +0200

    Implement new api routes

--- helpers/api_routes.jsx
+++ helpers/api_routes.jsx
@@ -1,6 +1,31 @@
+/*
+// jeziic
+https://www.ffos.unios.hr/projekti/glagopedija/API/v1/jezici
+// grupe jezik
+https://www.ffos.unios.hr/projekti/glagopedija/API/v1/grupe?jezik=hr
+// grupa jezik
+https://www.ffos.unios.hr/projekti/glagopedija/API/v1/grupa?id=2&jezik=en
+// kategorije grupa
+https://www.ffos.unios.hr/projekti/glagopedija/API/v1/kategorije?grupa=3&jezik=hr
+// kategorija grupa
+https://www.ffos.unios.hr/projekti/glagopedija/API/v1/kategorija?id=11&jezik=hr
+https://www.ffos.unios.hr/projekti/glagopedija/API/v1/zapisi?kategorija=1&jezik=hr
+https://www.ffos.unios.hr/projekti/glagopedija/API/v1/zapis?id=1&jezik=pl
+https://www.ffos.unios.hr/projekti/glagopedija/API/v1/poruke?jezik=en
+https://www.ffos.unios.hr/projekti/glagopedija/API/v1/poruka?kljuc=imeRegister_poruka&jezik=hr
+https://www.ffos.unios.hr/projekti/glagopedija/API/v1/popisliterature
+*/
+
+//root
 export const rootUrl = 'https://oziz.ffos.hr/nastava20192020/nmilic_19/v1';
-export const jezici = '/jezici';
-export const jezicneGrupe = '/grupe?jezik='
-export const stavke = (id, lang) => {
-	return `/kategorije?kategorija=${id}&jezik=${lang}`
-}
+// routes
+export const langs = '/jezici';
+export const groups = '/grupe?jezik=';
+export const group = (id, lang) => `/grupa?id=${id}&jezik=${lang}`;
+export const categories = (group, lang) => `/kategorije?grupa=${group}&jezik=${lang}`;
+export const category = (group, lang) => `/kategorije?grupa=${group}&jezik=${lang}`;
+export const records = (category, lang) => `/zapisi?kategorija=${category}&jezik=${lang}`;
+export const record = (id, lang) => `/zapis?id=${id}&jezik=${lang}`;
+export const messages = (lang) => `/poruke?jezik=${lang}`;
+export const message = (key, lang) => `poruka?kljuc=${key}&jezik=${lang}`;
+export const literature = `/popisliterature`;
